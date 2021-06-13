701f5e30a8193e9987eec11c36fb0040df466008
Apsaravigamage <apsaravigamage@gmail.com>
2021-06-13 17:51:16 +0530

    Food Order

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu section start-->
+        <div class="Menu">
+            Menu Gose Here
+        </div>
+        <!-- Menu section End-->
+
+        <!-- Main content section start-->
+        <div class="Menu">
+            Main content section Gose Here
+        </div>
+        <!-- Main content section End-->
+
+        <!-- Footer section start-->
+        <div class="Menu">
+            Footer section Gose Here
+        </div>
+        <!-- Footer section End-->
+    </body>
+</html>
