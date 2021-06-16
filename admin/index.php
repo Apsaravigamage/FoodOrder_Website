586ac26a528f0574bac3d240e06c59193c474733
Apsaravigamage <apsaravigamage@gmail.com>
2021-06-16 09:04:20 +0530

    wrapper class

--- admin/index.php
+++ admin/index.php
@@ -6,19 +6,25 @@
     <body>
         <!-- Menu section start-->
         <div class="Menu">
-            Menu Gose Here
+            <div class="wrapper">
+                 Menu Gose Here
+            </div> 
         </div>
         <!-- Menu section End-->
 
         <!-- Main content section start-->
         <div class="Menu">
-            Main content section Gose Here
+            <div class="wrapper">
+                Main content section Gose Here
+            </div> 
         </div>
         <!-- Main content section End-->
 
         <!-- Footer section start-->
         <div class="Menu">
-            Footer section Gose Here
+            <div class="wrapper">
+                <p class="text-center">2021 Pizza Shop Developed by - <a href="a"> Gamage </a></p>
+            </div>   
         </div>
         <!-- Footer section End-->
     </body>
