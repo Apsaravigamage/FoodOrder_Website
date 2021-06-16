c4f8b0852e9c5902d41ad3be697b92c7657888c4
Apsaravigamage <apsaravigamage@gmail.com>
2021-06-16 15:21:51 +0530

    create admin manage page

---
+++ admin/manage-admin.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu section start-->
+        <div class="Menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
+            </div> 
+        </div>
+        <!-- Menu section End-->
+
+        <!-- Main content section start-->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+
+                
+            </div> 
+        </div>
+        <!-- Main content section End-->
+
+        <!-- Footer section start-->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 Pizza Shop Developed by - <a href="a"> Gamage </a></p>
+            </div>   
+        </div>
+        <!-- Footer section End-->
+    </body>
+</html>
