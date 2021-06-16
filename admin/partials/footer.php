2d283776b612627069f6132aaa4bb9b52ed89472
Apsaravigamage <apsaravigamage@gmail.com>
2021-06-16 15:33:01 +0530

    creat manu and footer files

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer section start-->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 Pizza Shop Developed by - <a href="a"> Gamage </a></p>
+            </div>   
+        </div>
+        <!-- Footer section End-->
+    </body>
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu section start-->
+        <div class="Menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
+            </div> 
+        </div>
+        <!-- Menu section End-->
